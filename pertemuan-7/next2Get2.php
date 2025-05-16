fa8a314b9c153c9a667d5206e8d9b2493c891206
Mohammad Fadhol <mfadhol07@gmail.com>
2025-05-16 14:28:05 +0700

    adding isset GET method to avoid illegal accessing data

--- pertemuan-7/next2Get2.php
+++ pertemuan-7/next2Get2.php
@@ -1,3 +1,16 @@
+<?php
+// cek apakah tidak ada data di $_GET
+if( !isset($_GET["Nama"]) || 
+    !isset($_GET["NRP"]) ||
+    !isset($_GET["Universitas"]) ||
+    !isset($_GET["Gambar"]))
+    {
+    // redirect (memindahkan user dari sebuah halaman ke halaman lain)
+    // redirect dalam kasus ini untuk kickback user yang input page secara manual
+    header("Location: next1Get2.php");
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,7 +24,7 @@
         <li><img src="img/<?= $_GET ["Gambar"];?>"></li>
         <li><?= $_GET ["Nama"]; ?></li>
         <li><?= $_GET ["NRP"]; ?></li>
-        <li><?= $_GET ["Universitas"] ?></li>
+        <li><?= $_GET ["Universitas"] ?></li>    
     </ul>
 
 <a href="next1Get2.php">Kembali ke Daftar Mahasiswa</a>
